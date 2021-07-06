ed9cb3c7145298c3759094d45476959f86a1142d
cknabb <74315464+cknabb@users.noreply.github.com>
2021-07-06 18:26:05 +0200

    Add project files.

---
+++ Number.ConApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Number.ConApp
+{
+    class Program
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            Console.WriteLine("Number App!");                    // Set as Startup Projekt
+            string input, output;
+            long number;
+
+            //Eingabe (E)
+            do
+            {
+                Console.Write("Geben Sie eine Zahl ein: ");
+                input = Console.ReadLine();
+
+            } while (long.TryParse(input, out number) == false);
+
+            //Verarbeitung (V)
+
+            if (Number.Logic.NumberProperty.IsPerfect(number))
+            {
+                output = $"{number} ist eine perfekte Zahl";
+            }
+            else
+            {
+                output = $"{number} ist KEINE perfekte Zahl";
+            }
+
+            //Ausgabe
+
+            Console.WriteLine( output);
+
+        }
+    }
+}
---
+++ Number.Logic/NumberProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Number.Logic
+{
+    public static class NumberProperty // static kann nie eine Instanz haben. Nur statische Methoden zb. für mathematische.
+    {
+        public static bool IsPerfect(long number)
+        {
+            long sum = 1;
+
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number %i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum == number;
+        }
+
+    }
+}
